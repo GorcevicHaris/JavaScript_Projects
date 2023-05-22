267440e52b02542fe1be708bd11df1c69910a985
GorcevicHaris <harisgorcevic1000@gmail.com>
2023-05-22 11:24:20 +0200

    vezbanje

--- app.js
+++ app.js
@@ -2314,3 +2314,465 @@
 // console.log(somearray);
 // //NA ISTI NACIN DODAJEMO I U OBJEKAT 
 // // OVDE SMO UBACILI U JEDAN OBJEKAT JOS JEDAN OBJEKAT I U TAJ OBJEKAT DODALI JOB
+
+//======================================================================================
+
+// let somearray = [
+//   {name:'hamza',username:'gorcevic',age:1},
+//   {name:'hamza',username:'gorcevic',age:10},
+//   {name:'hamza',username:'gorcevic',age:100},
+//   {name:'hamza',username:'gorcevic',age:1000}
+
+// ];
+//  somearray.splice(0,3)
+
+// console.log(somearray);
+// //POMOCU SPLICE METODE MOZEMO IZBACIVATI ELEMENTE KOJE ZELIMO IZ NIZ OBJEKATA
+
+//=============================================================================
+
+// let somearray = [
+//   {name:'hamza',id:33,username:'gorcevic',age:1},
+//   {name:'hamza',id: 34,username:'gorcevic',age:10},
+//   {name:'hamza',id:55 ,username:'gorcevic',age:100},
+//   {name:'hamza',id:1,username:'gorcevic',age:1000}
+
+// ];
+
+// let newArray = somearray.filter((niz)=> niz.id % 2 === 0)
+// console.log(newArray);
+
+//===========================================================
+
+// prvoIme = 'haris'
+// drugoIme = 'gvido'
+// console.log(`${prvoIme} i ${drugoIme}` );
+//===========================================================
+// The global variable
+// const watchList = [
+//   {
+//     "Title": "Inception",
+//     "Year": "2010",
+//     "Rated": "PG-13",
+//     "Released": "16 Jul 2010",
+//     "Runtime": "148 min",
+//     "Genre": "Action, Adventure, Crime",
+//     "Director": "Christopher Nolan",
+//     "Writer": "Christopher Nolan",
+//     "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+//     "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+//     "Language": "English, Japanese, French",
+//     "Country": "USA, UK",
+//     "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//     "Metascore": "74",
+//     "imdbRating": "8.8",
+//     "imdbVotes": "1,446,708",
+//     "imdbID": "tt1375666",
+//     "Type": "movie",
+//     "Response": "True"
+//   },
+//   {
+//     "Title": "Interstellar",
+//     "Year": "2014",
+//     "Rated": "PG-13",
+//     "Released": "07 Nov 2014",
+//     "Runtime": "169 min",
+//     "Genre": "Adventure, Drama, Sci-Fi",
+//     "Director": "Christopher Nolan",
+//     "Writer": "Jonathan Nolan, Christopher Nolan",
+//     "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+//     "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+//     "Language": "English",
+//     "Country": "USA, UK",
+//     "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+//     "Metascore": "74",
+//     "imdbRating": "8.6",
+//     "imdbVotes": "910,366",
+//     "imdbID": "tt0816692",
+//     "Type": "movie",
+//     "Response": "True"
+//   },
+//   {
+//     "Title": "The Dark Knight",
+//     "Year": "2008",
+//     "Rated": "PG-13",
+//     "Released": "18 Jul 2008",
+//     "Runtime": "152 min",
+//     "Genre": "Action, Adventure, Crime",
+//     "Director": "Christopher Nolan",
+//     "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+//     "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+//     "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+//     "Language": "English, Mandarin",
+//     "Country": "USA, UK",
+//     "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+//     "Metascore": "82",
+//     "imdbRating": "9.0",
+//     "imdbVotes": "1,652,832",
+//     "imdbID": "tt0468569",
+//     "Type": "movie",
+//     "Response": "True"
+//   },
+//   {
+//     "Title": "Batman Begins",
+//     "Year": "2005",
+//     "Rated": "PG-13",
+//     "Released": "15 Jun 2005",
+//     "Runtime": "140 min",
+//     "Genre": "Action, Adventure",
+//     "Director": "Christopher Nolan",
+//     "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+//     "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+//     "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+//     "Language": "English, Urdu, Mandarin",
+//     "Country": "USA, UK",
+//     "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+//     "Metascore": "70",
+//     "imdbRating": "8.3",
+//     "imdbVotes": "972,584",
+//     "imdbID": "tt0372784",
+//     "Type": "movie",
+//     "Response": "True"
+//   },
+//   {
+//     "Title": "Avatar",
+//     "Year": "2009",
+//     "Rated": "PG-13",
+//     "Released": "18 Dec 2009",
+//     "Runtime": "162 min",
+//     "Genre": "Action, Adventure, Fantasy",
+//     "Director": "James Cameron",
+//     "Writer": "James Cameron",
+//     "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+//     "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+//     "Language": "English, Spanish",
+//     "Country": "USA, UK",
+//     "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+//     "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+//     "Metascore": "83",
+//     "imdbRating": "7.9",
+//     "imdbVotes": "876,575",
+//     "imdbID": "tt0499549",
+//     "Type": "movie",
+//     "Response": "True"
+//   }
+// ];
+
+// console.log(JSON.stringify(ratings));
+
+// let newarray = watchList.map((niz)=>{
+//   return {
+//     Title:niz.Title,
+//     imdbRating:niz.imdbRating
+//   }
+// })
+// newestarray = newarray.filter((element)=> element.imdbRating >= 8)
+// console.log(newestarray);
+
+//===========================================================================
+// let someObjectsInArray = [
+//   {name:'hamza',prezime:'gorcevic',godine:19},
+//   {name:'haris',prezime:'gorcevic',godine:18},
+//   {name:'dino',prezime:'cucak',godine:17}
+// ]
+
+// let somearray = someObjectsInArray.filter((nizovi)=>{
+//   return{
+//     name:nizovi.name,
+//     godine:nizovi.godine
+//   }
+// })
+// let noviniz = somearray.filter((element)=> element.godine > 17)
+
+// console.log(noviniz);
+//=========================================================================
+///// RAD SAD REDUCE METODOM SA OBJEKTIMA
+// let cars = [
+//   {id:30,model:'audi',year:2010},
+//   {id:11,model:'pasat',year:2018},
+//   {id:22,model:'mercedes',year:2020},
+//   {id:3,model:'bmw',year:2023}
+// ]
+
+// let somecars = cars.reduce((el1,el2)=>{
+//    el1.push({model:el2.model,year:el2.year})
+//    return el1
+// },[])
+// //OVAKO DODAJEMO KAD HOCEMO DA VRATIMO OBJEKAT 
+// console.log(somecars);
+//============================================================
+// let cars = [
+//   {id:30,model:'audi',year:2010},
+//   {id:11,model:'pasat',year:2018},
+//   {id:22,model:'mercedes',year:2020},
+//   {id:3,model:'bmw',year:2023}
+// ]
+
+// let somecars = cars.reduce((el1,el2)=>{
+//    el1.push(el2.model,el2.year)
+//    return el1
+// },[])
+// //OVO DODAJEMO SAMO U NIZ
+// console.log(somecars);
+
+//backtick
+
+
+// const nasobjekat ={
+//   name:'haris',
+//   duks:'zeleno',
+//   age: 18
+// }
+
+// function ourfistcallfunction(argumentfirst,argumentsecond) {
+//   console.log(`${argumentfirst} ima ${this.age} godina i ${argumentsecond} mu je ${this.duks}`);
+// }
+
+// ourfistcallfunction.call(nasobjekat,'haris','hukicu lopovu');
+// // ourfistcallfunction.apply(nasobjekat,['haris','hukicu lopovu']);
+
+
+// const somearray = {
+//   name:'haris',
+//   username:'gorcevic',
+//   age: 33,
+//   laptop:'apple',
+//   naocare:'vidne',
+//   secondname:'gvido'
+// }
+
+// function myFirstObject(){
+//   console.log(`${this.name} ${this.username}`);
+// }
+// myFirstObject.call(somearray)
+
+// function mySecondFunction(){
+//   console.log(`${this.age} ${this.laptop}`);
+// }
+// mySecondFunction.apply(somearray)
+
+
+
+
+// const users = [
+//   {
+//   id:3,
+//   godine:25,
+//   ime:'hare'
+// },
+// {
+//   id:5,
+//   godine:25,
+//   ime:'hare'
+// },
+// {
+//   id:10,
+//   godine:25,
+//   ime:'hare'
+// }
+// ]
+// let randomid = ((nekid)=>{
+//   nekiarray = users.filter((nizovi)=>
+//   nekid!==nizovi.id
+  
+//   )
+//   return nekiarray
+// }
+// )
+
+// console.log(randomid(3));
+//=======================================================
+
+// const users = [
+//   {
+//     id: 3,
+//     godine: 3,
+//     ime: 'hare'
+//   },
+//   {
+//     godine:33
+//   }
+// ];
+
+// function agefunction(){
+//   if(this.godine >= 18){
+//     console.log('punoletans si')
+//     console.log(`presao si ${this.godine - 18} godina od pulonetstva`);
+//   }else{
+//     console.log(`trebati jos ${18-this.godine} do punoletstva`);
+//   }
+// }
+
+// agefunction.call(users[1])
+
+//=======================================================================
+
+// const nasobjekat = {
+//   name:'haris',
+//   boja:'zelena',
+//   age: 18
+// }
+
+// function somefunction(argument1,argument2){
+//   console.log(`moje ime je ${this.name} imam auto marka ${argument1}
+//    njegova boja je ${this.boja} presao je ${argument2} kilometara`);
+// }
+// somefunction.call(nasobjekat,'golf',200)
+
+
+
+// const car = {
+//   brand: 'Tesla',
+//   sound: 'Vroom!',
+//   honk: function() {
+//     console.log(this.sound);
+//   }
+// };
+
+// const honkFn = car.honk.bind(car);
+// honkFn(); // Output: Vroom!
+// console.log(car);
+
+
+
+// const marke = [
+//   {
+//     car1:'bmw',
+//     car2:'audi'
+
+//   },
+//   {
+//     car1:'zeleni',
+//     car2:'plavo'
+//   }
+// ]
+
+// console.log(marke[0].car1,marke[1].car1);
+
+
+// const objekat =
+// {
+//   name:'haris',
+//   age:33
+// }
+
+// objekat['age']
+
+
+// console.log(objekat);
+//==================================
+
+
+// const objectpractice = {
+//   voda: 'maxi',
+//   laptop:  'apple',
+//   telefon: 'iphone'
+// }
+// const kljucevi = Object.keys(objectpractice)
+// console.log(kljucevi);
+
+// const vrednosti = Object.values(objectpractice)
+// console.log(vrednosti);
+
+// // Object.freeze(objectpractice)
+
+// Object.seal(objectpractice)
+// console.log(objectpractice);
+
+//KLOSURE KLOSURE KLOSURE KLOSURE////KLOSURE KLOSURE KLOSURE KLOSURE////KLOSURE KLOSURE KLOSURE KLOSURE//
+
+// let a = 10
+
+// function firstA(){
+//   let a = 5
+//   console.log(a);
+// }
+// function secondA(){
+//   let a = 7
+//   console.log(a);
+// }
+
+// function lastA(){
+//   console.log(a);
+// }
+// firstA()
+// secondA()
+// lastA()
+//====================================================
+
+// const objectpractice = {
+//   voda: 'rosa',
+//   laptop:  'apple',
+//   telefon: 'iphone'
+// }
+// const kljucevi = Object.keys(objectpractice)
+// const novikljuc = kljucevi[0]
+// const najnovikljuc = kljucevi[1]
+// // console.log(kljucevi);
+// // console.log(novikljuc);
+
+// const vrednosti = Object.values(objectpractice)
+// console.log(vrednosti);
+// const novivalues = vrednosti[0]
+// // console.log(novivalues);
+
+// console.log(`${novikljuc} i ${novivalues}`);
+// //OVO JE DRUGI NACIN POMOCU OBJECTS.KEYS I OBJECT.VALUES DA SPOJIMO KEY I VALUE MALO TEZI NACIN
+
+
+//===============================================================================
+// const users = [
+//   {
+//     id: 3,
+//     godine: 11,
+//     ime: 'hare'
+//   },
+//   {
+//     godine:33
+//   }
+// ];
+
+
+// function somefunction(){
+//   if(this.godine >= 18){
+//     console.log(`punoletan si i presao si ${this.godine - 18} godina od kako si punoletan`)
+//   } else{
+//     console.log(`treba ti jos ${18 - this.godine}  godina do punoletstva`);
+//   }
+// }
+// somefunction.call(users[0])
+// //ako je niz mora biti users pod indexima
+
+//===========================================================================================
+// let someArray = [1,2,3,4,5]
+
+// let somefunction = someArray.map((el)=> el * 8)
+
+// console.log(somefunction);
+
+//===================================================================================================
+
+// function createuser(){
+//   return{
+//     name:'haris'
+//   }
+// }
+
+// console.log(createuser());
+
+
+
+class Telefon {
+  constructor(el1,el2,el3){
+    this.marka = el1
+    this.gb = el2
+    this.model = el3
+  }
+}
+
+const noviobj = new Telefon('ajfon',64,'apple')
+console.log(noviobj);

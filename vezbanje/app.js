31e9a1414c0119dffaeb40c3a7e6aa9a8ac19a61
Haris Goricevic <124845679+GorcevicHaris@users.noreply.github.com>
2023-07-15 15:03:06 +0200

    Update app.js

--- vezbanje/app.js
+++ vezbanje/app.js
@@ -5441,7 +5441,38 @@ fetching()
 //   console.log(something4);
 // })
 
+//================================================================
+// const arr = [1, 2, 3, 4, 44, 2, 3, 32, 321];
 
+// const sum = (el) => {
+//   return el + 3;
+// };
+
+// const customMap = (array, callbackFunction) => {
+//   const newArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     newArray.push(callbackFunction(array[i]));
+//   }
+
+//   return newArray;
+// };
+
+// console.log(customMap(arr, sum));
+//=================================================================
+// let array = [1,2,3,4]
+
+// let someCounters = (el)=>{
+//   return el + 3
+// }
+
+// let someFunction = (niz,counter)=>{
+//   let pushArray = [];
+//   for (i = 0; i < niz.length; i++) {
+//     pushArray.push(counter(niz[i]))
+//   }
+//   return pushArray
+// }
+// console.log(someFunction(array,someCounters));
 
 
 

a82bfc31517c09511003fc44d5a1bb87ff1dd9e1
Haris Goricevic <124845679+GorcevicHaris@users.noreply.github.com>
2023-07-25 22:03:46 +0200

    Update app.js

--- vezbanje/app.js
+++ vezbanje/app.js
@@ -5642,6 +5642,22 @@ fetching()
 
 // }
 // console.log(osoba.nekaOsobina[0]);
+//     let buton = document.querySelector('.buton')
+// let paragraf = document.querySelector('.pg')
+// let maindiv = document.querySelector('.mainDiv')
+// let fetchData = ()=>{
+//   fetch(`https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,political,racist`)
+//   .then(response => response.json())
+//   .then((arg)=>{
+//     // console.log(arg)
+//     paragraf.textContent = arg.setup
+//   })
+// }
+// buton.addEventListener('click',(ev)=>{
+//   ev.preventDefault()
+//   fetchData()
+// })
+// fetchData()
 
 
 
